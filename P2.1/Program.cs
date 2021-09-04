5d7150facd271c67247836c69384aa7f5649cc4a
Karla262 <carlasiric@gmail.com>
2021-09-04 02:19:26 -0500

    Commit Programacion

---
+++ P2.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _01_Problema_2._1
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+           int CInicial=20000;
+           int CRecuperacion=2000;
+           int Anio=2005;
+           int NumeroAnios=6;
+           decimal depreciacionAnual=(CInicial-CRecuperacion)/NumeroAnios;
+           decimal depreciacionacumulada=0;
+           decimal valoranual=costoinicial;
+           int contador=1;
+           Console.WriteLine("Año           Depreciacion            Depreciacion Acomulada       ValorAnual");
+
+            while (contador <= NumeroAnios)
+            {
+                Anio=Anio+1;
+                depreciacionacumulada = depreciacionacumulada + depreciacioAnual;
+                valoranual = valoranual - depreciacioAnual;
+                Console.WriteLine("{0} ({1})            {2}               {3}                         {4}",contador, Anio, depreciacioAnual, depreciacionacomulada, valoranual);
+                contador = contador +1;
+                
+            }
+        }
+    }
+}
