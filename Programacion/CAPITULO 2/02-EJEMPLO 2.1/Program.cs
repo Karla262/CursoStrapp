758d9f3bfccebfafa0298a6d895b47cae60271a9
Karla262 <carlasiric@gmail.com>
2021-09-25 03:26:14 -0500

    actualizacion

---
+++ Programacion/CAPITULO 2/02-EJEMPLO 2.1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _02_EJEMPLO_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
