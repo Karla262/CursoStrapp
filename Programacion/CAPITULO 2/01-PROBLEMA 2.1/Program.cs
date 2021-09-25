471b10678f9d9e3c290ec01c95ecf5a8b7b87618
Karla262 <carlasiric@gmail.com>
2021-09-25 02:32:04 -0500

    Actualizacion

---
+++ Programacion/CAPITULO 2/01-PROBLEMA 2.1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01_PROBLEMA_2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int costoInicial = 20000;
+            int costoRecuperacion = 2000;
+            int anio = 2005;
+            int numeroAnios = 6;
+            decimal depreciacionAnual = (costoInicial - costoRecuperacion) / numeroAnios;
+            decimal depreciacionAcumulada = 0;
+            decimal valorAnual = costoInicial;
+            int contador = 1;
+            Console.WriteLine("Año          Depreciación        Depreciación Acumulada      ValorAnual");
+
+            while (contador <= numeroAnios)
+            {
+                anio = anio + 1;
+                depreciacionAcumulada = depreciacionAcumulada + depreciacionAnual;
+                Console.WriteLine("{0}  ({1})        {2}        {3}     {4}", contador, anio, depreciacionAnual, depreciacionAcumulada, valorAnual);
+                contador = contador + 1;
+            }
+        }
+    }
+}
