8a77c7fc20cfd1026c1fff64e82af6d2e4866c6d
Karla262 <carlasiric@gmail.com>
2021-09-27 10:24:54 -0500

    Actualización

---
+++ Programacion/CAPITULO 2/11-SINTAXIS/Program.cs
@@ -0,0 +1,198 @@
+﻿using System;
+
+namespace Sintaxis 
+{
+    class Program
+    {
+
+        // DECLARACION DE VARIABLES GLOBALES
+        
+        
+        static int variableEntero1;
+        static int variableEntero2 = 1;
+        static int variableEntero3, variableEntero4;
+        static int variableEntero5 = 10, variableEntero6 = 11;
+        static int variableEntero7, variableEntero8 = 10;
+
+        // TIPO DE DATOS PRIMITIVOS ¿INVESTIGAR QUE ES UN TIPO DE DATO PRIMITIVO? (VALUE TYPES)
+        // Se llama tipo primitivo o tipo elemental a los tipos de datos originales de un lenguaje de programación
+        // esto es, aquellos que nos proporciona el lenguaje y con los que podemos construir tipos de datos abstractos 
+        // y estructuras de datos. Generalmente ejemplos de tipos primitivos son: Char Int Float
+        static int vEntero;         // APLICA PARA DATOS DE TIPO ENTERO ¿INVESTIGAR CAPACIDADES DE UN INT?
+        static double vDoueble;     // APLICA PARA DATOS DE TIPO DECIMAL ¿INVESTIGAR CAPACIDADES DE UN DOUEBLE?
+        static decimal vDecimal;    // APLICA PARA DATOS DE TIPO DECIMAL ¿INVESTIGAR CAPACIDADES DE UN DECIMAL?
+        static float vfloat;        // APLICA PARA DATOS DE TIPO DECIMAL ¿INVESTIGAR CAPACIDADES DE UN FLOAT?
+        static string vString;      // APLICA PARA DATOS DE TIPO CADENA
+        static bool vBool;          // APLICA PARA DATOS DE TIPO BOOLEANO (FALSO O VERDADERO)
+        static DateTime vfecha;     // APLICA PARA DATOS DE TIPO FECHA; ¿INVESTIGAR SI ES TIPO DE DATO PRIMITIVO O TIPO DE DATO POR REFERENCIA?
+
+        // TIPO DE DATOS DE REFERENCIA (REFERENCE TYPE)
+        Object vObject;      // APLICA PARA OBJETOS EN GENERAL
+
+
+        // ASI SE DECLARA EL METODO MAIN
+        static void Main(string[] args)  // NO LLEVA ;
+        {
+            // DECLARACION DE VARIABLES LOCALES
+            int vLocalEntero1;
+            int vLocalEntero2 = 10;
+            int vLocalEntero3, vLocalEntero4;
+            int vLocalEntero5 = 10, vLocalEntero6;
+
+            // PARA ACCEDER A UNA VARIABLE GLOBAL
+            VEntero = 10;
+
+
+
+            // OPERACIONES ARITMETICAS
+            // SUMA
+            vEntero = vEntero + 5;          // TERMINA EN ;
+            // RESTAS
+            vEntero = vEntero - 5;          // TERMINA EN ;
+            // MULTIPLACIONES
+            vEntero = vEntero * 5;          // TERMINA EN ;
+            // DIVISIONES
+            vEntero = vEntero / 5;          // TERMINA EN ;
+            // RESIDUOS
+            vEntero = vEntero % 5;          // TERMINA EN ;
+
+
+            // OPERACIONES LOGICAS (COMPARACION)
+            int vLocalEntero7 = 10;                 // TERMINA EN ;
+            bool resultado = vLocalEntero7 > 3;         // TERMINA EN ; // OPERACION TIPO MAYOR QUE, GENERA UN RESPUESTA DE CIERTO O FALSO
+            bool resultado2 = vLocalEntero7 < 3;        // TERMINA EN ; // OPERACION TIPO MENOR QUE, GENERA UN RESPUESTA DE CIERTO O FALSO
+            bool resultado3 = vLocalEntero7 != 3;       // TERMINA EN ; // OPERACION TIPO DIFERENTE DE, GENERA UN RESPUESTA DE CIERTO O FALSO
+            bool resultado4 = vLocalEntero7 == 3;       // TERMINA EN ; // OPERACION TIPO IGUAL, GENERA UN RESPUESTA DE CIERTO O FALSO
+
+
+
+            // TERMINA EN ; // OPERACION DE RELACION AND (Y): SI A ES VERDADERO Y B ES VERDADERO EL RESULTADO ES VERDADERO
+            bool resultado5 = vLocalEntero7 > 3 && vLocalEntero7 != 3;
+            // TERMINA EN ; // OPERACION DE RELACION OR (O): SI A ES VERDADERO O B ES FALSO EL RESULTADO ES VERDADERO
+            bool resultado5 = vLocalEntero7 > 3 || vLocalEntero7 != 3;
+
+
+            // TERMINA EN ; // OPERACION DE NEGACION (O): SI A ES VERDADERO, EL RESULTADO ES FALSO, VICEVERSA
+            bool resultado5 = !resultado5;
+
+
+
+            // LAS CONDICIONES SE UTILIZAN EN LAS ESTRUCTURA DE CONTROL, ESTOS NO LLEVAN ;
+
+            // IF (SOLO UN IF), EL IF SE ALIMENTA DE UN VALOR CIERTO O FALSO
+
+            if(resultado)  // ------> ESTO PUEDE SER UNa EXPRESION BOOLEANA (vLocalEntero7 > 3); (vLocalEntero7 > 3 && vLocalEntero7 != 3)
+            {
+                // AQUI SE EJECUTA SI LA CONDICION FUE VERDADERA
+            }
+
+
+            // IF-ELSE (SE EJECUTA UNO O EL OTRO)
+            if(resultado)
+            {
+                // AQUI SE EJECUTA SI LA CONDICION FUE VERDADERA
+            }
+            else
+            {
+                // AQUI SE EJECUTA SI LA CONDICION FUE FALSA
+            }            
+
+            // WHILE EL while SE ALIMENTA DE UN VALOR CIERTO O FALSO, NO LLEVA ;
+            while(resultado)  // ------> ESTO PUEDE SER UN EXPRESION BOOLEANA (vLocalEntero7 > 3); (vLocalEntero7 > 3 && vLocalEntero7 != 3)
+            {
+                // ESTO ES LO QUE SE A REPETIR MIENTRA LA CONDICION SE VERDADERA
+                // instrucciones
+                // Declarar variables
+                // Puedo hacer operaciones aritmeticas, logica, relacion
+            }
+
+
+            // DO-WHILE SE ALIMENTA DE UN VALOR CIERTO O FALSO, ESTE SI LLEVA ; AL FINAL DE WHILE
+            do{
+                // ESTO ES LO QUE SE A REPETIR MIENTRA LA CONDICION SE VERDADERA
+                // instrucciones
+                // Declarar variables
+                // Puedo hacer operaciones aritmeticas, logica, relacion
+            }while(resultado);
+
+
+
+            // FOR TIENE UNA ESTRUCTURA: INICIALIZACION; CONDICION; INCREMENTO, NO LLEVA ;
+            for(int iteracion = 0; iteracion < 10; iteracion++)
+            {
+                // ESTO ES LO QUE SE A REPETIR MIENTRA LA CONDICION SE VERDADERA
+                // instrucciones
+                // Declarar variables
+                // Puedo hacer operaciones aritmeticas, logica, relacion
+            }
+            
+        }   // NO LLEVA ;
+
+
+
+
+        // ASI SE DECLARA UN METODO ESTATICO Y SIN RETORNO DE DATO (void)
+        static void Metodo(int parametro1, int parametro2)     // ESTOS SE LLAMAN PARAMETROS (parametro1, parametro2)
+        {
+            // DECLARACION DE VARIABLES LOCALES
+            int vLocalEntero;
+            int vLocalEntero = 10;
+            int vLocalEntero1, vLocalEntero2;
+            int vLocalEntero3 = 10, vLocalEntero4;
+
+
+        }
+
+        // METODOS SIN PARAMETRO Y SIN RETORNO DE DATO (void)
+        static void MetodoSinParametros()
+        {
+
+            // DECLARACION DE VARIABLES LOCALES
+            int vLocalEntero;
+            int vLocalEntero = 10;
+            int vLocalEntero1, vLocalEntero2;
+            int vLocalEntero3 = 10, vLocalEntero4;
+
+
+        }
+
+
+        // METODO CON PARAMETRO Y CON RETORNO DE DATO (int)
+        static int ObtenerEdad(DateTime fechaNacimiento)
+        {
+            // RETURNO RESULTADO DEL METODO
+            return new TimeSpan(DateTime.Now.Ticks - fechaNacimiento.Ticks).GetYear;
+        }
+    }
+
+    
+    // ASI SE DECLARA UNA CLASE
+    public class Empleado
+    {
+
+        // ESTO SE LLAMAN PROPIEDADES
+        // EN LAS PROPIEDADES SE PUEDEN UTILIZAR TIPO DE DATOS PRIMITIVO O TIPO DE DATOS POR REFERENCIA
+        public int NumeroEmpleado { get; set; }    
+        public string Nombre { get; set; }
+        public bool Activo {set; get; }
+
+        // ESTE ES UNA PROPIEDAD DE TIPO DE DATO POR REFERENCIA
+        public Direccion Direccion {set; get;}
+
+
+        // ESTO ES UN METODO DE UNA CLASE
+        public string ObtenerNombre()
+        {
+            return this.Nombre;
+        }
+
+    } 
+
+    public class Direccion
+    {
+
+        public string Calle { get; set; }
+        public string NumeroExterior {get; set;}
+
+    }
+}
---
+++ Programacion/CAPITULO 2/12-POO/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace _12_POO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creación de un objeto persona 
+            Persona persona1 = new Persona();
+            persona1.IdPersona = 1;
+            persona1.Nombre = "Juanito"; 
+            persona1.ApellidoPaterno ="Perez";
+            persona1.ApellidoMaterno ="Perez";
+            persona1.CorreoElectronico = "juanito@hotmail.com";
+            persona1.CURP = "PEPJ20210412HTPDJDJD";
+            persona1.FechaNacimiento = new Datetime(1999, 1, 20);
+            persona1.Nacionalidad = "MEXICANA"; 
+            persona1.Sexo = 'M';
+            persona1.Telefono = " 5578963214";
+           
+
+            //Se inicializa primero el objeto dirección
+            persona1.Direccion = new Direccion();
+            persona1.Direccion.Calle = "Av Coyoacan";
+            persona1.Direccion.NumeroExterior = "1035";
+            persona1.Direccion.NumeroInterior = "11";
+
+            int edad = persona1.ObtenerEdad();
+
+            //cREACION DE UN EMLPLEADO
+            Empleado empleado1 = new Empleado();
+            empleado1.IdPersona = 1;
+            empleado1.IdEmpleado = 1;
+            empleado1.Nombre = "Juanito"; 
+            empleado1.ApellidoPaterno ="Perez";
+            empleado1.ApellidoMaterno ="Perez";
+            empleado1.CorreoElectronico = "juanito@hotmail.com";
+            empleado1.CURP = "PEPJ20210412HTPDJDJD";
+            empleado1.FechaNacimiento = new Datetime(1999, 1, 20);
+            empleado1.Nacionalidad = "MEXICANA"; 
+            empleado1.Sexo = 'M';
+            empleado1.Telefono = " 5578963214";
+            empleado1.FechaIngreso = new Datetime(2021, 1, 1);
+            empleado1.Horario = "8-3";
+            empleado1.NumeroSeguroSocial = "124-28-96";
+            empleado1.Puesto = "PROGRAMADOR";
+            empleado1.RFC = "PEPJ202104";
+            empleado1.SalarioDiario = "400";
+            empleado1.TotalFaltas = 0;
+            
+            //Se inicializa primero el objeto dirección
+            empleado1.Direccion = new Direccion();
+            empleado1.Direccion.Calle = "Av Coyoacan";
+            empleado1.Direccion.NumeroExterior = "1035";
+            empleado1.Direccion.NumeroInterior = "11";
+
+            int edadEmpleado = empleado1.ObtenerEdad();
+            int antiguedad = empleado1.ObtenerAntiguedad();
+        }
+    }
+    public class Persona
+    {
+        public int IdPersona { get; set;}
+        public string Nombre { get; set;}
+        public string ApellidoPaterno { get; set;}
+        public string ApellidoMaterno { get; set;}
+        public char Sexo { get; set;}
+        public string CURP { get; set;}
+        public string Telefono { get; set;}
+        public string CorreoElectronico { get; set;}
+        public Datetime FechaNacimiento {get; set;}
+        public string Nacionalidad { get; set;}
+        public Direccion Direccion { get; set;}
+        public int ObtenerEdad(){
+            //ALGORITMO CALCULAR EDAD
+            //.........
+            int edad = 20; //Este se calcula con el algoritmo
+            retunr edad;
+        }
+    }
+
+    public class Direccion
+    {
+        public int IdDireccion { get; set;}
+        public string Calle { get; set;}
+        public string NumeroExterior { get; set;}
+        public string NumeroInterior { get; set;}
+        public string Colonia { get; set;}
+        public string Municipio { get; set;}
+        public string Estado { get; set;}
+        public string CodigoPostal { get; set;}
+    }
+    public class Empleado: Persona
+    {
+        public int IdEmpleado {get; set;}
+        public string RFC {get; set;}
+        public string Puesto {get; set;}
+        public Datetime FechaIngreso {get; set;}
+        public decimal SalarioDiario {get; set;}
+        public string NumeroSeguroSocial {get; set;}
+        public string Horario {get; set;}
+        public int TotalFaltas {get; set;}
+
+        public int ObtenerAntiguedad()
+        {
+            //ALGORITMO CALCULAR ANTIGUEDAD TOMANDO LA FECHA DE INGRESO
+            //.........
+            //this.FechaIngreso = "2021-09-14";
+            int antiguedad = 10; //Este se calcula con el algoritmo
+            retunr antiguedad;
+        }
+
+        public void Registra()
+        {
+
+        }
+
+        public void Actualiza()
+        {
+
+        }
+
+        public void Eliminar()
+        {
+
+        }
+
+    }
+}
