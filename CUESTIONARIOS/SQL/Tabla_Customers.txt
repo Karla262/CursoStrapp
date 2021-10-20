2fc7a079e730297e029fbc5d7a843998d85605fc
Karla262 <carlasiric@gmail.com>
2021-10-19 23:17:50 -0500

    dapper

---
+++ CUESTIONARIOS/SQL/Tabla_Customers.txt
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo.[Customers](
+[CustomerID] INT   IDENTITY (1, 1) NOT NULL,
+[FirstName] NVARCHAR (MAX) NULL,
+[LastName] NVARCHAR (MAX) NULL,
+[Email] NVARCHAR (MAX) NULL,
+CONSTRAINT [PK_dbo.Customers] PRIMARY KEY CLUSTERED ([CustomerID] ASC)
+);
+
+SELECT * FROM [dbo.[Customers]
---
+++ PROYECTOS/DAPPER/MvcDapper/Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcDapper.Models;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+
+namespace MvcDapper.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly ILogger<CustomerController> _logger;
+        private readonly IConfiguration configuration;
+
+        public CustomerController(ILogger<CustomerController> logger, IConfiguration config)
+        {
+            _logger = logger;
+            configuration = config;
+        }
+
+        public IActionResult Index()
+        {
+            List<Customer> customers = new List<Customer>();
+            string cadenaConexion = configuration.GetConnectionString("CustomerConnection");
+            using (IDbConnection db = new SqlConnection(cadenaConexion))
+            {
+                
+                customers = db.Query<Customer>("Select * From Customers").ToList();
+            }
+            return View(customers);
+        }
+    }
+}
