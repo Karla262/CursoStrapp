3a5b8cf93f71a7a90f9701d80a307b2b8a389611
Karla262 <carlasiric@gmail.com>
2021-11-05 01:06:25 -0600

    actualizar

---
+++ Programacion/CAPITULO 3/3.3.3.Instrucciones de lectura/InstruccionesLectura/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InstruccionesLectura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce el valor de numeros:");
+            string entrada = Console.ReadLine();
+            int numeros = int.Parse(entrada);
+
+            Console.WriteLine("Introduce el valor de Horas:");
+            entrada = Console.ReadLine();
+            int horas = int.Parse(entrada);
+
+            Console.WriteLine("Introduce el valor de Tasas:");
+            entrada = Console.ReadLine();
+            int tasas = int.Parse(entrada);
+
+            Console.WriteLine("Los valores son: " + numeros + ", " + horas+", " + tasas);
+
+        }
+    }
+}
---
+++ Programacion/CAPITULO 3/3.3.4.Instrucciones de escritura/IndtruccionesEscritura/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IndtruccionesEscritura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 100, B = 200, C = 300;
+            Console.WriteLine("El valor de las Variables es: A = " + A + "  B = " + B + "  C = " + C );
+        }
+    }
+}
